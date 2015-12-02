229a885639ad6c1e31937381bf5eea376055524d
sswstow <sswistow@o2.pl>
2015-12-02 19:39:50 +0100

    Update server.c

--- src/server.c
+++ src/server.c
@@ -6,19 +6,64 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
 
 const int PORT = 8080;
 const int BUFFER_SIZE = 1024;
+struct sockaddr_in address;
+char * root = "example";
+
+void* newClient(void* newSock)
+{
+	
+	int new_socket = *((int*) newSock);
+	// create a buffer to keep data from request
+	char *buffer = (char*) malloc(BUFFER_SIZE);
+
+	if (new_socket < 0) {
+         	exit(1);
+      	}
+	
+      	// Receive data from client
+      	recv(new_socket, buffer, BUFFER_SIZE, 0);
+
+      	// extracting a method and a path from the first line
+      	char * method = strtok(buffer, " ");
+      	char * path = strtok(NULL, " ");
+
+      	printf("Socket: [%s] %s\n", method, path);
+
+      	char url[256] = "";
+      	strcat(url, root);
+      	strcat(url, path);
+
+      	FILE * file = fopen(url, "r");
+
+      	if (file != NULL){
+
+         	const int size = 256;
+         	char line[size];
+
+         	while (fgets(line, size, file) != NULL) {
+
+            	// write response
+            	write(new_socket, line, strlen(line));
+
+         	};
+
+         	fclose(file);
+
+      	}
+	
+      // close the connection
+      close(new_socket);
+
+}
+
 
 int main(int argc, char *argv[]) {
 
-   char * root = argv[1];
-
    socklen_t address_length;
-
-   // create a buffer to keep data from request
-   char *buffer = (char*) malloc(BUFFER_SIZE);
-   struct sockaddr_in address;
 
    int create_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -35,49 +80,15 @@ int main(int argc, char *argv[]) {
 
    while (1) {
 
-      if (listen(create_socket, 10) < 0) {
+      	if (listen(create_socket, 10) < 0) {
          exit(1);
-      }
+      	}
 
-      int new_socket = accept(create_socket, (struct sockaddr *) &address, &address_length);
-
-      if (new_socket < 0) {
-         exit(1);
-      }
-
-      // Receive data from client
-      recv(new_socket, buffer, BUFFER_SIZE, 0);
-
-      // extracting a method and a path from the first line
-      char * method = strtok(buffer, " ");
-      char * path = strtok(NULL, " ");
-
-      printf("Socket: [%s] %s\n", method, path);
-
-      char url[256] = "";
-      strcat(url, root);
-      strcat(url, path);
-
-      FILE * file = fopen(url, "r");
-
-      if (file != NULL){
-
-         const int size = 256;
-         char line[size];
-
-         while (fgets(line, size, file) != NULL) {
-
-            // write response
-            write(new_socket, line, strlen(line));
-
-         };
-
-         fclose(file);
-
-      }
-
-      // close the connection
-      close(new_socket);
+      	int new_socket = accept(create_socket, (struct sockaddr *) &address, &address_length);
+	
+	pthread_t newThread;
+	
+	pthread_create(&newThread, NULL, newClient, (void*)&new_socket);
 
    }
 
